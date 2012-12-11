f2a2839cbf0192fe41deb8a7ec30a635f954b409
benjamin <benjamin@ubuntu.(none)>
2012-12-11 08:20:20 -0800

    no confirmation

--- new_machine.sh
+++ new_machine.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo apt-get install git
-sudo apt-get install puppet
+sudo apt-get install git -y
+sudo apt-get install puppet -y
 git clone git://github.com/benjaminchanming/puppet-emacs.git  
 mv ~/puppet-emacs /etc/puppet/modules/emacs
 sudo puppet apply --verbose -e "include emacs"
