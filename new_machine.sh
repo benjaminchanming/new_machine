17037bb32f76cf428bf49c094684fb6570394ab2
benjaminchanming <benjaminchanming@gmail.com>
2012-12-12 08:02:10 +0800

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install git -y
+sudo apt-get install vim-nox -y
 sudo apt-get install puppet -y
 git clone git://github.com/benjaminchanming/puppet-emacs.git  
 mv ~/puppet-emacs /etc/puppet/modules/emacs
